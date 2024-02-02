e17709f8cffc8e37c3e360a5ff25cce5947c4905
nbs3 <nbs3@g5c25.cin.ufpe.br>
2024-02-02 12:49:32 -0300

    graph por terminar

---
+++ LISTA5/graph.cpp
@@ -0,0 +1,96 @@
+#include <iostream>
+#include <list>
+using namespace std;
+
+class Graph{
+    public:
+        list <int> *matrix;
+        int numEdge;
+        int *Mark;
+        int countNode;
+
+        Graph *create_graph(int n){
+            Graph *g = new Graph;
+            g->Mark = new int[n];
+            g->matrix = new list[n];
+            for(int i{}; i < n; i++){
+                g->matrix[i] = new list[n];
+            }
+            g->countNode = 0;
+            g->numEdge = 0;
+            return g;
+        }
+};
+
+int first(Graph *g, int v, int count){
+    for(int i{}; i <= count - 1; i++){
+        if(g->matrix[v][i] != 0){
+            return i;
+        }
+    }
+    return count;
+}
+
+int next(Graph *g, int v, int w, int count){
+    for(int i = w + 1; i <= count - 1; i++){
+        if(g->matrix[v][i] != 0){
+            return i;
+        }
+    }
+    return count;
+}
+
+void setEdge(Graph *g, int i, int j, int wt){
+    if(wt == 0); break;
+    if(g->matrix[i][j] = 0){
+        g->numEdge++;
+    }
+    g->matrix[i][j] = wt;
+}
+
+void delEdge(Graph *g, int i, int j){
+    if(g->matrix[i][j] != 0){
+        g->numEdge--;
+    }
+    g->matrix[i][j] = 0;
+}
+
+void DFS(Graph *g, int v, int count){
+    preVisit(g, v);
+    setMArk(g, v, "VISITED");
+    w = first(g, v, count);
+    while(w < count){
+        if(getMark(g, w) == "UNVISITED"){
+            DFS(g, w, count);
+            w = next(g, v, w);
+        }
+    }
+    posVisit(g, v);
+}
+
+void graphTraverse(Graph *g, int count){
+    for(v = 0; v <= count - 1; v++){
+        setMark(g, v, "UNVISITED");
+    }
+    for(v{}; v <= count - 1; v++){
+        if(getMark(g, v) == "UNVISITED"){
+            DFS(g, v);
+        }
+    }
+}
+
+// void graphTraverse2(Graph *g, int count){
+//     for(v = 0; v < count - 1; v++){
+//         setMArk(g, v, "UNVISITED");
+//     }
+//     for(v = 0; v < count - 1; v++){
+//         if(getMark(g, v) == "UNVISITED"){
+//             DFS(g, v);
+//         }
+//     }
+// }
+
+int main(void){
+    return 0;
+}
+
