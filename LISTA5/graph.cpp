fdb116f29876cd8f7581212cd88a385d93be6b70
nbs3 <nbs3@g2c12.cin.ufpe.br>
2024-02-06 13:04:42 -0300

    Grafo implementado finalmente

--- LISTA5/graph.cpp
+++ LISTA5/graph.cpp
@@ -6,31 +6,25 @@
 using namespace std;
 
 class Graph{
-
     public:
-        list <int> *matrix;
-        int numEdge;
-        int *Mark;
-        int countNode;
-
         Graph *create_graph(int n){
             Graph *g = new Graph;
-            g->Mark = new int[n];
-            g->matrix = new list[n];
+            g->Mark = new bool[n];
+            g->matrix = new int[n];
             for(int i{}; i < n; i++){
-                g->matrix[i] = new list[n];
+                g->matrix[i] = new int[n];
             }
-            g->countNode = 0;
             g->numEdge = 0;
+            g->countNode = n;
             return g;
         }
     
-        void graphTraverseDFS(Graph *g, int count){
-            for(v = 0; v <= count - 1; v++){
-                setMark(g, v, "UNVISITED");
+        void graphTraverseDFS(Graph *g){
+            for(v{}; v <= g->countNode - 1; v++){
+                setMark(g, v, false);
             }
             for(v{}; v <= count - 1; v++){
-                if(getMark(g, v) == "UNVISITED"){
+                if(getMark(g, v) == false){
                     DFS(g, v);
                 }
             }
@@ -48,28 +42,32 @@ class Graph{
 
 
     private:
+        int **matrix;
+        int numEdge;
+        bool *Mark;
+        int countNode;
 
-        int first(Graph *g, int v, int count){
-            for(int i{}; i <= count - 1; i++){
+        int first(Graph *g, int v){
+            for(int i{}; i <= g->countNode - 1; i++){
                 if(g->matrix[v][i] != 0){
                     return i;
                 }
             }
-            return count;
+            return g->countNode;
         }
 
-        int next(Graph *g, int v, int w, int count){
-            for(int i = w + 1; i <= count - 1; i++){
+        int next(Graph *g, int v, int w){
+            for(int i = w + 1; i <= g->countNode - 1; i++){
                 if(g->matrix[v][i] != 0){
                     return i;
                 }
             }
-            return count;
+            return g->countNode;
         }
 
         void setEdge(Graph *g, int i, int j, int wt){
             if(wt == 0); break;
-            if(g->matrix[i][j] = 0){
+            if(g->matrix[i][j] == 0){
                 g->numEdge++;
             }
             g->matrix[i][j] = wt;
@@ -81,14 +79,30 @@ class Graph{
             }
             g->matrix[i][j] = 0;
         }
-}
-        void DFS(Graph *g, int v, int count){
+
+        void setMark(Graph *g, int v, bool var){
+            if(var == true)
+                g->Mark[v] = true;
+            else
+                g->Mark[v] = false;
+        }
+
+        bool getMark(Graph *g, int v){
+            if(g->Mark[v] == true){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+
+        void DFS(Graph *g, int v){
             cout << "Iniciando a visita do nó:" << v << endl;
-            setMArk(g, v, "VISITED");
-            w = first(g, v, count);
-            while(w < count){
-                if(getMark(g, w) == "UNVISITED"){
-                    DFS(g, w, count);
+            setMark(g, v, true);
+            int w = first(g, v);
+            while(w < g->countNode){
+                if(getMark(g, w) == false){
+                    DFS(g, w);
                     w = next(g, v, w);
                 }
             }
@@ -98,37 +112,38 @@ class Graph{
         void BFS(Graph *g, int start){
             queue<int> Q;
             Q.push(start);
-            setMark(g, start, "VISITED");
+            setMark(g, start, true);
 
             while(Q.size() > 0){
             int v = Q.pop();
             cout << "o nó:" << v << "Foi removido" << endl;
-            int w = first(g, v, g->countNode);
+            int w = first(g, v);
             }
 
             while(w < g->countNode){
-                if(getMark(g, w) == "UNVISITED"){
-                    setMark(g, w, "VISITED");
+                if(getMark(g, w) == false){
+                    setMark(g, w, true);
                     Q.push(w);
                 }
-            }    
+                w = next(g, v, w);
+            }
+            cout << "BFS FINALIZADO" << endl;    
         }
 
+        void toposort(Graph *g, int v, stack s){
+            setMark(g, v, true);
+            int w = first(g, v);
 
+            while(w < g->countNode){
+                if(getMark(g, w) == false){
+                    toposort(g, w, s);
+                }
+                w = next(g, v, w);
+            }
+            s.push(v);
+        }
+}
 
-
-
-
-// void graphTraverse2(Graph *g, int count){
-//     for(v = 0; v < count - 1; v++){
-//         setMArk(g, v, "UNVISITED");
-//     }
-//     for(v = 0; v < count - 1; v++){
-//         if(getMark(g, v) == "UNVISITED"){
-//             DFS(g, v);
-//         }
-//     }
-// }
 
 int main(void){
     return 0;
