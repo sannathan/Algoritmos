5b4ec2f16174038410aa6285f59d8f0f277f6cf6
sannathan <nathansantos1713@gmail.com>
2024-02-22 11:48:41 -0300

    Dijkstra

--- AP11/Dijkstra.cpp
+++ AP11/Dijkstra.cpp
@@ -1,14 +1,20 @@
 #include <iostream>
 #include <queue>
+#include <bits/stdc++.h> 
 
 using namespace std;
+
+struct ComparePairs{
+    bool operator()(const pair<int, int>& p1, const pair<int, int>& p2){
+        return p1.second > p2.second;
+    }
+};
 
 class Graph{
     public:
         Graph (int n){
             //D = new int[n];
             P = new int [n];
-            H = new priority_queue<int, int, int>[n];
             Mark = new bool [n];
             matrix = new int*[n];
             for(int i{}; i < n; i++){
@@ -27,7 +33,7 @@ class Graph{
         int *P;
         int **matrix;
         bool *Mark;
-        priority_queue<int, int, int> *H;
+        priority_queue<pair<pair<int, int>, int>, vector<pair<pair<int, int>, int>>, ComparePairs> H;
 
         void setMark(int v, bool var){
             Mark[v] = var;
@@ -63,12 +69,12 @@ class Graph{
 
                 setMark(i, false);
             }
-            H[1].emplace(s, s, 0);
+            H.push({{s, s}, 0});
             D[s] = 0;
             for(int i = 0; i < countNode; i++){
                 do{
-                    H[1].pop(p, v);
-                    if()
+                    H.pop();
+                    //Colocar um if aqui
                 }while(!(GetMark(v) == false));
                 setMark(v, true);
                 P[v] = p;
