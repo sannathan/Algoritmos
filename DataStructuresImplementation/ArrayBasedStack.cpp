5c273431432bac08569209485f462f127487ec40
Nathan <nathansantos1713@gmail.com>
2025-11-20 11:44:55 -0300

    feat: implementing Stack abstract class and  Array Based stack

---
+++ DataStructuresImplementation/ArrayBasedStack.cpp
@@ -0,0 +1,52 @@
+#include "ArrayBasedStack.h"
+#include <cassert>
+
+const int defaultSize = 100;
+template <typename E> class AStack: public Stack<E>
+{
+private:
+    int maxSize;
+    int top;
+    E *listArray;
+
+public:
+    AStack(int size = defaultSize)
+    {
+        maxSize = size;
+        top = 0;
+        listArray = new E[size];
+    }
+
+    ~AStack()
+    {
+       delete [] listArray;
+    }
+
+    void clear()
+    {
+        top = 0;
+    }
+
+    void push(const E& it)
+    {
+        assert(top != maxSize, "Stack is full");
+        listArray[top++] = it;
+    }
+
+    E pop()
+    {
+        assert(top != 0, "Stack is empty");
+        return listArray[--top];
+    }
+
+    const E& topValue() const
+    {
+        assert(top != 0, "Stack is empty");
+        return listArray[top-1];
+    }
+
+    int length() const
+    {
+        return top;
+    }
+};
