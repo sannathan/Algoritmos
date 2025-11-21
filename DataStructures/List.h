3c65a66e505606ec31e1ced48a9a805865b9b74a
Nathan <nathansantos1713@gmail.com>
2025-11-21 13:27:17 -0300

    feat: adding LinkedList implementation

---
+++ DataStructures/List.h
@@ -0,0 +1,29 @@
+#ifndef LIST_H
+#define LIST_H
+
+template <typename E> class List {
+private:
+  void operator=(const List &) {}
+  List(const List &) {}
+
+public:
+  List() {}
+  virtual ~List() {}
+
+  virtual void clear() = 0;
+  virtual void insert(const E &item) = 0;
+  virtual void append(const E &item) = 0;
+  virtual E remove() = 0;
+  virtual void moveToStart() = 0;
+  virtual void moveToEnd() = 0;
+  virtual void prev() = 0;
+  virtual void next() = 0;
+  virtual int lenght() const = 0;
+  virtual int currPos() const = 0;
+  virtual void moveToPos(int pos) = 0;
+  virtual const E &getValue() const = 0;
+};
+
+#include "ArrayBasedList.cpp"
+
+#endif
