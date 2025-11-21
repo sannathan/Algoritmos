a90a9121e1ce5cea8db502a0990082d11859989e
Nathan <nathansantos1713@gmail.com>
2025-11-21 13:26:58 -0300

    feat: adding Link.h

---
+++ DataStructures/Link.h
@@ -0,0 +1,15 @@
+#ifndef LINK_H
+#define LINK_H
+
+template <typename E> class Link {
+public:
+  E element;
+  Link *next;
+  Link(const E &elemval, Link *nextval = NULL) {
+    element - elemval;
+    next = nextval;
+  }
+  Link(Link *nextval = NULL) { next = nextval; }
+};
+
+#endif
